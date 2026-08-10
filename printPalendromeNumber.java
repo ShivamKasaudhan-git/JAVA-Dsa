f4d18e2632cf28600e16cafd905712fccdaaec8d
Shivam Kasaudhan <shivamkasaudhan8286@gmail.com>
2026-08-10 15:30:29 +0530

    Create printPalendromeNumber.java

---
+++ printPalendromeNumber.java
@@ -0,0 +1,19 @@
+public class printPalendromeNumber {
+    public static void main(String[] args) {
+        int number = 12321; // Change this number to check for different values
+        int originalNumber = number;
+        int reversedNumber = 0;
+
+        while (number != 0) {
+            int digit = number % 10;
+            reversedNumber = reversedNumber * 10 + digit;
+            number /= 10;
+        }
+
+        if (originalNumber == reversedNumber) {
+            System.out.println(originalNumber + " is a palindrome number.");
+        } else {
+            System.out.println(originalNumber + " is not a palindrome number.");
+        }
+    }
+}
