6668207c7b27ed4350195f8b873d6f5c0d81d631
Shivam Kasaudhan <shivamkasaudhan8286@gmail.com>
2026-08-19 15:54:14 +0530

    Create CreatelistOfStudent.java

---
+++ CreatelistOfStudent.java
@@ -0,0 +1,10 @@
+public class CreatelistOfStudent {
+    public static void main(String[] args) {
+        String[] students = { "Alice", "Bob", "Charlie", "David", "Eve" };
+
+        System.out.println("List of Students:");
+        for (String student : students) {
+            System.out.println(student);
+        }
+    }
+}
