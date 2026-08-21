c8ea23f94b2438b4eef61ddaa2fe9f152aadff53
Shivam Kasaudhan <shivamkasaudhan8286@gmail.com>
2026-08-21 15:31:52 +0530

    Create printString.java

---
+++ printString.java
@@ -0,0 +1,8 @@
+public class printString {
+    public static void main(String[] args) {
+        String[] strArray = { "Apple", "Orange", "Banana", "Pineapple" };
+        for (int i = 0; i < strArray.length; i++) {
+            System.out.println(strArray[i]);
+        }
+    }
+}
